8a4a4f6fda00c015fb0b3f7cd91b5e21edee9c19
PromoART <archer88@mail.ru>
2016-05-20 21:48:17 +0300

    Добавил запись в файл лога а также черновик юнит теста для создания лог-сообщения

---
+++ LogServer/Listeners/LogListener.cs
@@ -0,0 +1,39 @@
+﻿using CoreLib.Encryption;
+using CoreLib.Listeners;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LogServer.Listeners
+{
+    public class LogListener : CommandListener
+    {
+        public LogListener(int listenPort, IPEndPoint localTcpEp) : base(listenPort, localTcpEp){
+        }
+        protected override void Parse(byte[] data)
+        {
+            string comname = "save-to-log";
+            string decryptXml = Encoding.ASCII.GetString(Encrypter.DecryptData(data));
+            var xml = new XmlDocument();
+            xml.LoadXml(decryptXml);
+            XmlNodeList nodeList = xml.GetElementsByTagName("Command");
+            if (xml.GetElementsByTagName("Command").ToString()==comname)
+            {
+                var xmlNode = nodeList.Item(0);
+            }
+        }
+        public void WriteLog()
+        {
+            string path=System.IO.Directory.GetCurrentDirectory().ToString()+@"logger.txt";
+            byte[] txtbuff = new byte[4096];
+            FileStream str = new FileStream("path", FileMode.Append, FileAccess.Write);
+            str.WriteAsync(txtbuff, 0, (int)str.Length);
+        }
+    }
+}
